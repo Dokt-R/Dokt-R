9b3dfea7d7a884e9dec48f00e6053289c11b8747
Nick Papadopoulos <nikolaospap94@gmail.com>
2022-09-10 18:40:12 +0300

    Successful walk through directories

--- server/massclean.py
+++ server/massclean.py
@@ -1,49 +1,57 @@
 import logging
 import timeit
-from os import listdir, walk
-from os.path import isfile, join, splitext
+from os import walk, mkdir
+from os.path import exists, join, splitext
 import pandas as pd
 
-logging.basicConfig(filename="log.txt", level=logging.DEBUG,
-                    format="%(asctime)s %(message)s", filemode='w')
+# Logger Setup
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s:%(levelname)s -> %(message)s')
+
+file_handler = logging.FileHandler('clean.log', mode='w')
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+
+# Start timer and declare constant variables
 starttime = timeit.default_timer()
 
-
-class FileProperties:
-    def __init__(self, file) -> None:
-        self.filename = file.split('.')[0]
-        self.codename = file[0]
-        self.year = int(file[1:5])
-        self.doy = int(file[5:8])
-        self.date = pd.to_datetime(self.year * 1000 + self.doy, format='%Y%j')
-        self.daterange = pd.date_range(self.date, periods=86400, freq='s')
-
-
-# Script
-
-
-PATH = ".\\dat"
-PATH_CSV = ".\\csv"
-
-filelist = [f for f in listdir(PATH) if isfile(join(PATH, f))]
+PATH = r".\dat"
 COLUMNS = ['ch1', 'ch2', 'ch3', 'ch4', 'ch5', 'ch6']
 
 for subdir, dirs, files in walk(PATH):
     for file in files:
-        print(join(subdir, file))
-        print(file)
-        # f = FileProperties(file)
+        filePath = join(subdir, file)
+        csvName = splitext(file)[0]+'.csv'
 
-        # df = pd.read_csv(
-        #     PATH+file, usecols=[4, 5, 6, 7, 8, 9], names=COLUMNS)
+        newDir = subdir.replace('dat', 'csv')
+        if not exists(newDir):
+            mkdir(newDir)
+        csvPath = join(newDir, csvName)
 
-        # df.insert(loc=0, column='moment', value=f.daterange)
-        # df.insert(loc=0, column='codename', value=f.codename)
+        try:
+            codename = file[0]
+            year = int(file[1:5])
+            doy = int(file[5:8])
+        except ValueError as e:
+            logger.error('%s\nLocation: %s', e, filePath)
+        except Exception:
+            logger.exception('Something unexpected happened!')
+        else:
+            date = pd.to_datetime(year * 1000 + doy, format='%Y%j')
+            daterange = pd.date_range(date, periods=86400, freq='s')
+            df = pd.read_csv(
+                filePath, usecols=[4, 5, 6, 7, 8, 9], names=COLUMNS)
+            df.insert(loc=0, column='moment', value=daterange)
+            df.insert(loc=0, column='codename', value=codename)
 
-        # df.to_csv(PATH_CSV+filename+'.csv', index=False)
-        # print(f.filename, f.year, f.doy, f.date)
-        # print(df)
+            df.to_csv(csvPath, index=False)
+
+            logger.info('%s finished loading', filePath)
+
         break
 
 endtime = timeit.default_timer()
