e13adb7a6c284840c21d475f9650dc8beef36617
Nick Papadopoulos <nikolaospap94@gmail.com>
2022-09-19 20:53:45 +0300

    Get Frame Values Function

--- ElsemData/Python/analysis.py
+++ ElsemData/Python/analysis.py
@@ -19,11 +19,12 @@ Tsallis:
     PHYSICAL REVIEW E 66, 046134 (2002)
 '''
 
+from scipy.stats import linregress
 import math
 import pandas as pd
 import numpy as np
+from scipy.stats import linregress, spearmanr
 # from scipy.signal import detrend
-# from scipy.stats import linregress, spearmanr
 
 
 # ? Probably delete self.bins
@@ -69,6 +70,13 @@ class SeriesAnalysis:
         '''Returns total number of rows in the dataset'''
         return len(self.df.index)
 
+    def frame_values(self, frame_number):
+        '''Return the values of at a given frame window
+        E.g: Series[1024:2048]'''
+        start = frame_number * self.inc
+        stop = start + self.frame_size
+        return self.df[start:stop]
+
     def entropy_analysis(self):
         '''OUTPUT:
         shannon = Calculates Shannon Information (AntEntropy, i.e., opposite to
@@ -79,10 +87,7 @@ class SeriesAnalysis:
         '''
         entropies = pd.DataFrame()
         for frame_number in range(self.total_frames):  # forward frame sliding
-
-            start = frame_number * self.inc
-            stop = start + self.frame_size
-            frame = self.df[start:stop]
+            frame = self.frame_values(frame_number)  # ! Test
             for column in frame:
                 hist, bin_edges = np.histogram(
                     frame[column])  # Histogram Graph
@@ -113,7 +118,139 @@ class SeriesAnalysis:
         entropies = entropies.sort_values(by=['channel'], kind='mergesort')
         return entropies
 
+    def rs1(self, ts, n):
+        '''
+        RS1 calculates Rescale Range Analysis (by Hurst) of a time-series on a 
+        given vector of scales
 
-CSV_PATH = 'C:\\Users\\Doktar\\Desktop\\git\\Dokt-R\\ElsemData\\RawData\\A2020001.csv'
-analysis = SeriesAnalysis(CSV_PATH)
-entropies = analysis.entropy_analysis()
+        INPUTS:
+        ts            is the input time-series vector
+        n             is the vector of scales on which analysis will be performed
+              is the a-priori knowledge about the model (fBm or fGn) which TS fillows
+
+        OUTPUTS:
+        log_n         is the vector of scales' logarithms
+        log_rs        is the vector of mean R/S's logarithms
+        '''
+
+        N = len(ts)
+
+        log_n = []
+        log_RS = []
+
+        for m in np.arange(0, len(n)):
+            iters = np.floor(N / n[m])  # (FIX)
+            if iters != 0:
+                r = []
+                s = []
+                rs = []
+                for nFrame in np.arange(0, iters):
+                    indx1 = int((nFrame) * n[m])
+                    indx2 = int((nFrame) * n[m] + n[m])
+                    x = ts[indx1:indx2]
+                    xm = np.mean(x)
+                    # mean of time series
+
+                    yN = x - xm
+                    # mean-adjusted time series
+
+                    ynN = np.cumsum(yN)  # cumulative deviate series
+
+                    xnm = xm
+                    # Ayto den to ksekatharizei stoy Eftaxia to
+                    # kai einai kapws diaforetiko stoy Qian
+
+                    r.append(max(ynN) - min(ynN))
+
+                    s.append(math.sqrt(((sum((x - xnm) ** 2))) / n[m]))
+
+                    if s[int(nFrame)] != 0:
+                        rs.append(r[int(nFrame)] / s[int(nFrame)])
+                    else:
+                        s[int(nFrame)] = np.finfo(float).eps
+                        rs.append(r[int(nFrame)] / s[int(nFrame)])
+
+            log_RS.append(
+                math.log2(np.mean(np.real(rs)))
+            )  # Log, Log10, Log2 may be used as equivalents
+            # Log, Log10, Log2 may be used as equivalents
+            log_n.append(math.log2(n[m]))
+        return log_RS, log_n
+
+    def hurst_analysis(self):
+        # Get maximum scale value, if desirable to have one
+
+        Sc_max = 256
+        Sc_max_pow2 = math.floor(math.log2(Sc_max))
+
+        n_w = math.floor((len(self) - self.frame_size) / self.inc) + 1
+
+        h = []
+        log_a = []
+        rr = []
+
+        # New Code
+        for frame_number in range(self.total_frames):
+            frame = self.frame_values(frame_number)
+            for column in frame:
+                n = column.shape()  # ?
+                n_max = math.floor(math.log2(n))
+
+                n_arr = []
+                for j in np.arange(1, min(Sc_max_pow2, n_max) - 1):
+                    n_arr.append(2 ** (j + 2))
+
+                # Calculation of R/S
+                log_rs, log_n = rs1(column, n_arr)
+
+                # Linear Fit of R/S
+
+                # This is the one that gives correct estimates of r^2
+                if sum(np.isnan(log_rs)) > 0:  # ! Needs Fix for NaN Values
+                    # H[ind1:ind2] = NaN.*ones(w,1)
+                    # log_a[ind1:ind2] = NaN.*ones(w,1)
+                    # rr[ind1:ind2] = NaN.*ones(w,1)
+                    print("F")
+                else:
+                    p = np.polyfit(log_n, log_rs, 1)
+                    # p_val = np.polyval(p, log_rs)
+                    # log_rs_eval = np.polyval(p, log_n)
+                    r2 = linregress(log_n, log_rs).rvalue ** 2
+                    while ind1 < ind2:
+                        h.append(p[0])
+                        log_a.append(p[1])
+                        rr.append(r2)
+                        ind1 += 1
+
+        # Original code (Will be deleted)
+        for q in np.arange(1, n_w + 1):
+            ind1 = (q - 1) * self.inc
+            ind2 = (q - 1) * self.inc + self.frame_size
+            X_tot_w = self.df[ind1:ind2]
+            n = len(X_tot_w)  # length of time series
+            n_max = math.floor(math.log2(n))
+
+            n_arr = []
+            for j in np.arange(1, min(Sc_max_pow2, n_max) - 1):
+                n_arr.append(2 ** (j + 2))
+
+            # Calculation of R/S
+            log_RS, log_n = rs1(X_tot_w, n_arr)
+
+            # Linear Fit of R/S
+
+            p = np.polyfit(log_n, log_RS, 1)
+            # p_val = np.polyval(p, log_RS)
+            # log_RS_eval = np.polyval(p, log_n)
+            r2 = linregress(log_n, log_RS).rvalue ** 2
+            while ind1 < ind2:
+                h.append(p[0])
+                log_a.append(p[1])
+                rr.append(r2)
+                ind1 += 1
+
+
+if __name__ == '__main__':
+    CSV_PATH = 'C:\\Users\\Doktar\\Desktop\\git\\Dokt-R\\ElsemData\\RawData\\A2020001.csv'
+    analysis = SeriesAnalysis(CSV_PATH)
+    entropies = analysis.entropy_analysis()
